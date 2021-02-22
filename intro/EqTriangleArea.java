98b60862e9253fa78835dd114b120d83839ef7a9
Ritwik Neema <ritwikneema651@gmail.com>
2021-02-22 17:12:35 +0530

    Added new ex

---
+++ intro/EqTriangleArea.java
@@ -0,0 +1,18 @@
+package intro;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class EqTriangleArea {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter side of triangle: ");
+
+        double side = input.nextDouble();
+        double area = (Math.sqrt(3)/4)*side*side;
+
+        System.out.println("The area is: " + area + " square units");
+
+        input.close();
+    }
+}
