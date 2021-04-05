0809501709df40dcd08753f79c37f826824633a7
Ritwik Neema <ritwikneema651@gmail.com>
2021-04-06 01:35:43 +0530

    Exceptions

---
+++ exception/ExceptionsIntro.java
@@ -0,0 +1,53 @@
+package exception;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ExceptionsIntro {
+    public static void main(String[] args) throws IOException {
+        try {
+            System.out.print("Enter a number: ");
+            Scanner input = new Scanner(System.in);
+            int num = input.nextInt();
+
+            if (num == 13) {
+                throw new Number13Exception("I don't like 13");
+            }
+
+            else if (num == 15) {
+                throw new Number15Exception("15 not allowed");
+            }
+        }
+        // In case when multiple catch blocks qualify to handle a single type of exception, it is the first catch block that catches
+        // A base class exception can handle all child class exceptions
+        catch (Exception e) {
+            if (e instanceof Number13Exception) {
+                System.out.println("Please don't enter 13 again");
+            }
+
+            if (e instanceof Number15Exception) {
+                System.out.println("15 is not allowed");
+            }
+        }
+    }
+}
+
+class MyException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public MyException(String s) {
+        super(s);
+    }
+}
+
+class Number13Exception extends MyException {
+    public Number13Exception(String S) {
+        super(S);
+    }
+}
+
+class Number15Exception extends MyException {
+    public Number15Exception(String S) {
+        super(S);
+    }
+}
---
+++ exception/UsingThrows.java
@@ -0,0 +1,38 @@
+package exception;
+
+import java.util.Scanner;
+
+public class UsingThrows {
+    public static void main(String[] args) {
+        // If a nested try doesn't have a catch, it searches in the outer try block, if not found, it terminates the program
+        try {
+            try {
+                DivideByZero d = new DivideByZero();
+                d.divideNumbers();
+            } finally {
+                System.out.println("Inner finally executed");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Caught!");
+        }
+    }
+}
+
+class DivideByZero {
+    private int a;
+    private int b;
+
+    public DivideByZero() {
+        System.out.println("Enter two numbers:");
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+    }
+
+    // One might as well omit the throws clause, it is included in the signature so
+    // as to help developers understand what exception might occur and thus needs
+    // handling
+    public void divideNumbers() throws ArithmeticException {
+        System.out.println(a / b);
+    }
+}
