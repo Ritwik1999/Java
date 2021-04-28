f1bf5ef9feb1e52171a3e1fcc44a4648b856e9ef
Ritwik Neema <ritwikneema651@gmail.com>
2021-04-28 11:42:54 +0530

    utf char ex changed

--- classexercises/aprilextwosix/UTFChars.java
+++ classexercises/aprilextwosix/UTFChars.java
@@ -7,28 +7,28 @@ public class UTFChars {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         int choice;
-        int charCode = 1013;
 
-        File obj = new File("classexercises/aprilextwosix/sample.txt");
+        File obj = new File("sample.txt");
         FileOutputStream fout = new FileOutputStream(obj, true);
-        OutputStreamWriter iwrite = new OutputStreamWriter(fout, "UTF-16");
         FileInputStream fin = new FileInputStream(obj);
-        InputStreamReader iread = new InputStreamReader(fin, "UTF-16");
 
         System.out.println();
         do {
             int option;
+            int dataToBeWritten;
             System.out.print("Enter 1 to write a UTF-16 character, or 2 to read all characters: ");
             option = Integer.parseInt(input.nextLine());
 
             switch (option) {
             case 1:
-                iwrite.write(charCode);
-                charCode++;
-                System.out.println("Written");
+                OutputStreamWriter iwrite = new OutputStreamWriter(fout, "UTF-16");
+                System.out.print("Enter data to be written: ");
+                dataToBeWritten = Integer.parseInt(input.nextLine());
+                iwrite.write(dataToBeWritten);
                 break;
 
             case 2:
+                InputStreamReader iread = new InputStreamReader(fin, "UTF-16");
                 int data;
                 while ((data = iread.read()) != -1) {
                     System.out.println("Code: " + data + "\tCharacter: " + (char) data);
@@ -43,8 +43,8 @@ public class UTFChars {
             choice = Integer.parseInt(input.nextLine());
         } while (choice == 1);
 
+        fout.close();
+        fin.close();
         input.close();
-        iwrite.close();
-        iread.close();
     }
 }
