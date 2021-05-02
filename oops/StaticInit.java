a330c8e74e027237b9a249ab5b2421522f0e1418
Ritwik Neema <ritwikneema651@gmail.com>
2021-05-02 19:26:14 +0530

    oops static block added

---
+++ oops/StaticInit.java
@@ -0,0 +1,34 @@
+/*
+Code a java program such that it outputs the area of a parallelogram with breadth B and height H. You should read the variables from the standard input.
+
+If B <= 0 or H <= 0, the output should be "java.lang.Exception: Breadth and height must be positive" without quotes.
+*/
+
+package oops;
+
+import java.util.*;
+
+public class StaticInit {
+    static int B;
+    static int H;
+    static boolean flag;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+        flag = true;
+        if (B <= 0 || H <= 0) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.print(area);
+        }
+    }
+}
